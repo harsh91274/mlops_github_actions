0c3638d1eed45f9f734a7995757354c790bdd736
harshvc91 <116171382+harshvc91@users.noreply.github.com>
2024-10-22 15:41:28 -0400

    data ingestion

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from typing import List
 HYPHEN_E_DOT='-e .'
 
 def get_requirements(file_path:str): -> List[str]
-    '''
+    """"
     This function will return the list of requirements
-    '''
+    """
     
     requirements=[]
     with open(file_path) as file_obj:
